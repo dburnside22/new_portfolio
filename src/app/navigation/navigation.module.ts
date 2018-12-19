3ce19407bead24d6b5eb3ea2f10dbae86d13b857
Dan Burnside <burnside@Daniels-MBP.attlocal.net>
2018-12-19 17:35:05 -0600

    created navgation module

---
+++ src/app/navigation/navigation.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+
+@NgModule({
+  declarations: [NavigationBarComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class NavigationModule { }
